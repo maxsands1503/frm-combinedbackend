0637ca9fc52bd0c76e1b09a6cdfa58e320688b2e
Maxwell Sands <maxwellsands@Maxwells-MacBook-Air.local>
2016-09-12 12:18:06 -0600

    Abandon all hope ye who enter here

--- queries/connectionsQueries.js
+++ queries/connectionsQueries.js
@@ -33,7 +33,7 @@ module.exports ={
   getOneConnection: function(id){
     return knex('connections').where('user_id', id).first().then(function(connection){
       return knex('list_users').where('conn_id', connection.id).then(function(lists){
-        return knex('lists').whereIn('id', lists.list_id).then(function(titles){
+        return knex('lists').where('id', lists.list_id).then(function(titles){
           return {
             connection: connection,
             lists: lists,
